19294903d74c386e9a5914df088dd7003886e67e
Raghavanaveen <your-email@example.com>
2025-04-18 21:13:04 +1000

    Initial commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hello from Docker!'));
+
+app.get('/', (req, res) => res.send('🚀 Hello from Kubernetes v2!'));
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
